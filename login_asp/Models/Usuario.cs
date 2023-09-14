8524e1ed3b72784ac44ad6f424fa7605821741e0
Johann3421 <1355430@senati.pe>
2023-09-14 12:43:46 -0500

    Mejorando mi codigo para que acepte el apellido

--- login_asp/Models/Usuario.cs
+++ login_asp/Models/Usuario.cs
@@ -9,6 +9,8 @@ public partial class Usuario
 
     public string? NombreUsuario { get; set; }
 
+    public string? Apellido { get; set; }
+
     public string? Correo { get; set; }
 
     public string? Clave { get; set; }
